14b255889160d9df942ab0cc96508ffd120996bc
fukaoi <goodakira7@gmail.com>
2018-10-27 02:27:11 +0900

    modified

--- src/hello-world.cc
+++ src/hello-world.cc
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "v8.h"
+#include "include/v8.h"
+#include "include/libplatform/libplatform.h"
 
 int main(int argc, char* argv[]) {
   v8::V8::InitializeDefaultLocation(argv[0]);
