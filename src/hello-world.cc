f3fad70141acbc33855f7060ec8f1b746fe3eb53
fukaoi <goodakira7@gmail.com>
2018-10-28 19:27:29 +0900

    debugging

--- src/hello-world.cc
+++ src/hello-world.cc
@@ -5,6 +5,7 @@
 #include "v8.h"
 
 int main(int argc, char* argv[]) {
+  printf("argv: %s\n", argv[0]);
   v8::V8::InitializeICUDefaultLocation(argv[0]);
   v8::V8::InitializeExternalStartupData(argv[0]);
   std::unique_ptr<v8::Platform> platform = v8::platform::NewDefaultPlatform();
