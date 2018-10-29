f5526902d53bd93b230f9527477e4b5fe8dba359
fukaoi <goodakira7@gmail.com>
2018-10-29 20:36:59 +0900

    setting ldflags

--- src/duk-js.cr
+++ src/duk-js.cr
@@ -6,7 +6,7 @@ end
 
 Say.hello("your name")
 
-@[Link(ldflags: "#{__DIR__}/jslib.o  -lstdc++  #{__DIR__}/libicui18n.so #{__DIR__}/libicuuc.so #{__DIR__}/libv8_libbase.so  #{__DIR__}/libv8.so")]
+@[Link(ldflags: "#{__DIR__}/jslib.o  -lstdc++ #{__DIR__}/libv8_libplatform.so #{__DIR__}/libicui18n.so #{__DIR__}/libicuuc.so #{__DIR__}/libv8_libbase.so  #{__DIR__}/libv8.so")]
 
 lib Say
   fun jslib : Void
