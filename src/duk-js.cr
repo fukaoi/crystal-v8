35b7c7a6c116bcdde37dc758e28b5198c4eca82e
fukaoi <goodakira7@gmail.com>
2018-10-29 18:47:22 +0900

    modified

--- src/duk-js.cr
+++ src/duk-js.cr
@@ -6,7 +6,7 @@ end
 
 Say.hello("your name")
 
-@[Link(ldflags: "#{__DIR__}/jslib.o")]
+@[Link(ldflags: "#{__DIR__}/jslib.o  -lstdc++  #{__DIR__}/libicui18n.so #{__DIR__}/libicuuc.so #{__DIR__}/libv8_libbase.so  #{__DIR__}/libv8.so")]
 
 lib Say
   fun jslib : Void
