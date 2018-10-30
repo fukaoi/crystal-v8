ca8f55949de788fa0429a6d3f327539e1c4b573b
fukaoi <goodakira7@gmail.com>
2018-10-30 18:48:49 +0900

    modified

--- src/jslib_test.c
+++ src/jslib_test.c
@@ -1,8 +1,7 @@
-// gcc src/jslib.o -lstdc++ ext/libv8_libplatform.so ext/libv8_libbase.so ext/libicui18n.so ext/libicuuc.so ext/libv8.so  src/jslib_test.c
+// gcc src/jslib.o -lstdc++ ext/libv8_libplatform.so ext/libv8_libbase.so ext/libicui18n.so ext/libicuuc.so ext/libv8.so  src/jslib_test.c -o src/jslib_test.o
 
 #include <string.h>
 
 void main() {
-  jslib("new Date()", "glue");
   jslib("new Date()", "glue");
 }
