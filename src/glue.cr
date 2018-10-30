ebbd3e621fd2b7b7e54eeb4969f7677716a191a9
fukaoi <goodakira7@gmail.com>
2018-10-30 19:04:24 +0900

    modified

--- src/glue.cr
+++ src/glue.cr
@@ -9,9 +9,6 @@ require "file_utils"
 # v8_use_external_startup_data = false
 # v8_static_library = true
 
-LIB_PATH = FileUtils.pwd + "/ext"
-
-
 @[Link(ldflags: " \
   #{__DIR__}/jslib.o -lstdc++ \
   #{__DIR__}/../ext/libicui18n.so \
@@ -21,15 +18,15 @@ LIB_PATH = FileUtils.pwd + "/ext"
   #{__DIR__}/../ext/libv8.so"
 )]
 lib Say
-  fun jslib(jscode : LibC::Char*, argv : LibC::Char*) : Void
+  fun jslib(jscode : LibC::Char*) : Void
 end
 
-!pp LibC.strlen("test")
-!pp LibC.strlen("てすとだお")
+#!pp LibC.strlen("test")
+#!pp LibC.strlen("てすとだお")
 
-Say.jslib("new Date()", "glue")
-# Say.jslib("const a = 10;let b = 10;a + b;", "glue")
-# Say.jslib("const fn = function(a){return a * 999};fn(10)", "glue2")
-Say.jslib("const fn = (a)=> {return a * 555};fn(10)", "glue2")
+#Say.jslib("new Date()")
+# Say.jslib("const a = 10;let b = 10;a + b;")
+Say.jslib("const fn = function(a){return a * 999};fn(10)")
+Say.jslib("const fn = (a)=> {return a * 555};fn(10)")
 
 
