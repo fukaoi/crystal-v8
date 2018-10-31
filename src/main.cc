6e0c7262f59b7abc85106021810126838bf7e364
fukaoi <goodakira7@gmail.com>
2018-10-31 16:10:44 +0900

    Refactoring split main and jslib func

--- src/main.cc
+++ src/main.cc
@@ -3,19 +3,11 @@
 #include <string.h>
 #include "libplatform/libplatform.h"
 #include "v8.h"
-int main(int argc, char* argv[]) {
-  // Initialize V8.
-  v8::V8::InitializeICUDefaultLocation(argv[0]);
-  v8::V8::InitializeExternalStartupData(argv[0]);
-  std::unique_ptr<v8::Platform> platform = v8::platform::NewDefaultPlatform();
-  v8::V8::InitializePlatform(platform.get());
-  v8::V8::Initialize();
+
+int jslib(char *code, v8::Isolate* isolate) {
   // Create a new Isolate and make it the current one.
-  v8::Isolate::CreateParams create_params;
-  create_params.array_buffer_allocator =
-      v8::ArrayBuffer::Allocator::NewDefaultAllocator();
-  v8::Isolate* isolate = v8::Isolate::New(create_params);
-  {
+
+  { 
     v8::Isolate::Scope isolate_scope(isolate);
     // Create a stack-allocated handle scope.
     v8::HandleScope handle_scope(isolate);
@@ -25,7 +17,7 @@ int main(int argc, char* argv[]) {
     v8::Context::Scope context_scope(context);
     // Create a string containing the JavaScript source code.
     v8::Local<v8::String> source =
-        v8::String::NewFromUtf8(isolate, "'Hello' + ', World!'",
+        v8::String::NewFromUtf8(isolate, code,
                                 v8::NewStringType::kNormal)
             .ToLocalChecked();
     // Compile the source code.
@@ -39,8 +31,27 @@ int main(int argc, char* argv[]) {
   }
   // Dispose the isolate and tear down V8.
   isolate->Dispose();
+  return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    // Initialize V8.
+  v8::V8::InitializeICUDefaultLocation(argv[0]);
+  v8::V8::InitializeExternalStartupData(argv[0]);
+  std::unique_ptr<v8::Platform> platform = v8::platform::NewDefaultPlatform();
+  v8::V8::InitializePlatform(platform.get());
+  v8::V8::Initialize();
+  v8::Isolate::CreateParams create_params;
+  create_params.array_buffer_allocator =
+  v8::ArrayBuffer::Allocator::NewDefaultAllocator();
+  v8::Isolate* isolate = v8::Isolate::New(create_params);
+  jslib("2 * 2", isolate);
+  jslib("10 + 20", isolate);
   v8::V8::Dispose();
   v8::V8::ShutdownPlatform();
   delete create_params.array_buffer_allocator;
   return 0;
 }
+
+
