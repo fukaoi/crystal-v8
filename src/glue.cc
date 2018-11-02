837b41cf9d17cef7af0836f7f355cded04c94275
fukaoi <goodakira7@gmail.com>
2018-11-02 17:53:02 +0900

    modified

--- src/glue.cc
+++ src/glue.cc
@@ -8,6 +8,10 @@
 
 extern "C"
 {
+
+  v8::Isolate *isolate;
+  int flags = 0;
+
   int jsparser(const char *code, v8::Isolate *isolate)
   {
     {
@@ -15,28 +19,27 @@ extern "C"
       // Create a stack-allocated handle scope.
       v8::HandleScope handle_scope(isolate);
       // Create a new context.
-      v8::Local<v8::Context> context = v8::Context::New(isolate);
-      context->Enter();
-      // Enter the context for compiling and running the hello world script.
-      v8::Context::Scope context_scope(context);
-      // Create a string containing the JavaScript source code.
-      v8::Local<v8::String> source =
-          v8::String::NewFromUtf8(isolate, code,
-                                  v8::NewStringType::kNormal)
-              .ToLocalChecked();
-      // Compile the source code.
-      v8::Local<v8::Script> script =
-          v8::Script::Compile(context, source).ToLocalChecked();
-      // Run the script to get the result.
-      v8::Local<v8::Value> result = script->Run(context).ToLocalChecked();
-      // Convert the result to an UTF8 string and print it.
-      v8::String::Utf8Value utf8(isolate, result);
-      printf("%s\n", *utf8);
+        std::cout << "#####################" << std::endl;
+        v8::Local<v8::Context> context = v8::Context::New(isolate);
+        std::cout << context->IsCodeGenerationFromStringsAllowed() << std::endl;
+
+        // Enter the context for compiling and running the hello world script.
+        v8::Context::Scope context_scope(context);
+        // Create a string containing the JavaScript source code.
+        v8::Local<v8::String> source =
+            v8::String::NewFromUtf8(isolate, code,
+                                    v8::NewStringType::kNormal)
+                .ToLocalChecked();
+        // Compile the source code.
+        v8::Local<v8::Script> script =
+            v8::Script::Compile(context, source).ToLocalChecked();
+        // Run the script to get the result.
+        v8::Local<v8::Value> result = script->Run(context).ToLocalChecked();
+        v8::String::Utf8Value utf8(isolate, result);
+        printf("%s\n", *utf8);
     }
     return 0;
   }
-
-  v8::Isolate *isolate;
 
   void jslib(char *code)
   {
@@ -47,18 +50,18 @@ extern "C"
     v8::V8::InitializePlatform(platform.get());
     v8::V8::Initialize();
 
-
-      v8::Isolate::CreateParams create_params;
+    v8::Isolate::CreateParams create_params;
       create_params.array_buffer_allocator =
           v8::ArrayBuffer::Allocator::NewDefaultAllocator();
-      if (!isolate) {
-       isolate = v8::Isolate::New(create_params);    
-       isolate->Enter();       
-      }    
-      delete create_params.array_buffer_allocator;
+    if (!isolate) {
+      std::cout << "*** Created isolate ***" << std::endl;
+      isolate = v8::Isolate::New(create_params);
+      //isolate->Enter();       
+    }    
+    delete create_params.array_buffer_allocator;
 
     jsparser(code, isolate);
-    // isolate->Dispose();
+ 
     v8::V8::Dispose();
     v8::V8::ShutdownPlatform();
   }
--- src/glue.cr
+++ src/glue.cr
@@ -26,6 +26,8 @@ end
 #!pp LibC.strlen("てすとだお")
 
 Say.jslib("new Date()")
+Say.jslib("new Date()")
+Say.jslib("new Date()")
 Say.jslib("const a = 10;let b = 10;a + b;")
 Say.jslib("class Person {
   constructor(name) {
@@ -38,7 +40,7 @@ Say.jslib("class Person {
     return this.name;
   }
 }; new Person('山田敬三').sayHello();")
-# Say.jslib("const fn = function(a){return a * 999};fn(10)")
+Say.jslib("const fn = function(a){return a * 999};fn(10)")
 Say.jslib("const fn = (a)=> {return a * 555};fn(10)")
 
 
