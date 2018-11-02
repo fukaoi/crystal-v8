b8331b590ac639a69b90a3f0bc1c7dcaa61eaf0e
fukaoi <goodakira7@gmail.com>
2018-11-02 18:49:08 +0900

    modified

--- src/glue.cc
+++ src/glue.cc
@@ -18,25 +18,22 @@ extern "C"
       v8::Isolate::Scope isolate_scope(isolate);
       // Create a stack-allocated handle scope.
       v8::HandleScope handle_scope(isolate);
-      // Create a new context.
-        std::cout << "#####################" << std::endl;
-        v8::Local<v8::Context> context = v8::Context::New(isolate);
-        std::cout << context->IsCodeGenerationFromStringsAllowed() << std::endl;
+      v8::Local<v8::Context> context = v8::Context::New(isolate);
 
-        // Enter the context for compiling and running the hello world script.
-        v8::Context::Scope context_scope(context);
-        // Create a string containing the JavaScript source code.
-        v8::Local<v8::String> source =
-            v8::String::NewFromUtf8(isolate, code,
-                                    v8::NewStringType::kNormal)
-                .ToLocalChecked();
-        // Compile the source code.
-        v8::Local<v8::Script> script =
-            v8::Script::Compile(context, source).ToLocalChecked();
-        // Run the script to get the result.
-        v8::Local<v8::Value> result = script->Run(context).ToLocalChecked();
-        v8::String::Utf8Value utf8(isolate, result);
-        printf("%s\n", *utf8);
+      // Enter the context for compiling and running the hello world script.
+      v8::Context::Scope context_scope(context);
+      // Create a string containing the JavaScript source code.
+      v8::Local<v8::String> source =
+          v8::String::NewFromUtf8(isolate, code,
+                                  v8::NewStringType::kNormal)
+              .ToLocalChecked();
+      // Compile the source code.
+      v8::Local<v8::Script> script =
+          v8::Script::Compile(context, source).ToLocalChecked();
+      // Run the script to get the result.
+      v8::Local<v8::Value> result = script->Run(context).ToLocalChecked();
+      v8::String::Utf8Value utf8(isolate, result);
+      printf("%s\n", *utf8);
     }
     return 0;
   }
@@ -51,17 +48,17 @@ extern "C"
     v8::V8::Initialize();
 
     v8::Isolate::CreateParams create_params;
-      create_params.array_buffer_allocator =
-          v8::ArrayBuffer::Allocator::NewDefaultAllocator();
-    if (!isolate) {
+    create_params.array_buffer_allocator =
+        v8::ArrayBuffer::Allocator::NewDefaultAllocator();
+    if (!isolate)
+    {
       std::cout << "*** Created isolate ***" << std::endl;
       isolate = v8::Isolate::New(create_params);
-      //isolate->Enter();       
-    }    
+    }
     delete create_params.array_buffer_allocator;
 
     jsparser(code, isolate);
- 
+
     v8::V8::Dispose();
     v8::V8::ShutdownPlatform();
   }
