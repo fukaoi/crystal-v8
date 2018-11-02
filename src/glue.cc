c70bcaba02c5e9f8f71af28beed7f4d2e3c66353
fukaoi <goodakira7@gmail.com>
2018-11-03 01:01:16 +0900

    add function init, fnalyze

--- src/glue.cc
+++ src/glue.cc
@@ -11,7 +11,34 @@ extern "C"
 
   v8::Isolate *isolate;
 
-  void init() {
+  int jsparser(const char *code, v8::Isolate *isolate)
+  {
+    {
+      v8::Isolate::Scope isolate_scope(isolate);
+      // Create a stack-allocated handle scope.
+      v8::HandleScope handle_scope(isolate);
+      v8::Local<v8::Context> context = v8::Context::New(isolate);
+
+      // Enter the context for compiling and running the hello world script.
+      v8::Context::Scope context_scope(context);
+      // Create a string containing the JavaScript source code.
+      v8::Local<v8::String> source =
+          v8::String::NewFromUtf8(isolate, code,
+                                  v8::NewStringType::kNormal)
+              .ToLocalChecked();
+      // Compile the source code.
+      v8::Local<v8::Script> script =
+          v8::Script::Compile(context, source).ToLocalChecked();
+      // Run the script to get the result.
+      v8::Local<v8::Value> result = script->Run(context).ToLocalChecked();
+      v8::String::Utf8Value utf8(isolate, result);
+      printf("%s\n", *utf8);
+    }
+    return 0;
+  }
+
+  void run(char *code)
+  {
     // Initialize V8.
     v8::V8::InitializeICUDefaultLocation("./bin/glue");
     v8::V8::InitializeExternalStartupData("./bin/glue");
@@ -22,43 +49,19 @@ extern "C"
     v8::Isolate::CreateParams create_params;
     create_params.array_buffer_allocator =
         v8::ArrayBuffer::Allocator::NewDefaultAllocator();
-      std::cout << "*** Created isolate ***" << std::endl;
-    isolate = v8::Isolate::New(create_params);
-    isolate->Enter();
-    delete create_params.array_buffer_allocator;
- 
-  }
-
-  int jsparser(const char *code, v8::Isolate *isolate)
-  {
+    if (!isolate)
     {
-      v8::Isolate::Scope isolate_scope(isolate);
-      v8::HandleScope handle_scope(isolate);
-      v8::Local<v8::Context> context = v8::Context::New(isolate);
-      context->Enter();
-      v8::Context::Scope context_scope(context);
-  
-      v8::Local<v8::String> source =
-          v8::String::NewFromUtf8(isolate, code,
-                                  v8::NewStringType::kNormal)
-              .ToLocalChecked();
-      v8::Local<v8::Script> script =
-          v8::Script::Compile(context, source).ToLocalChecked();
-      v8::Local<v8::Value> result = script->Run(context).ToLocalChecked();
-      v8::String::Utf8Value utf8(isolate, result);
-      printf("%s\n", *utf8);
+      std::cout << "*** Created isolate ***" << std::endl;
+      isolate = v8::Isolate::New(create_params);
     }
-    return 0;
-  }
+    delete create_params.array_buffer_allocator;
 
-  void run(char *code)
-  {
     jsparser(code, isolate);
 
-  }
-
-  void finalyze() {
     v8::V8::Dispose();
     v8::V8::ShutdownPlatform();
   }
+
+  void init() {}
+  void finalyze(){}
 }
