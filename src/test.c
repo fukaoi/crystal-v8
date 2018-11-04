129a9befecf978d43ce0983a29e4831a21cbd7e7
fukaoi <goodakira7@gmail.com>
2018-11-04 10:47:09 +0900

    Build shell test function

--- src/test.c
+++ src/test.c
@@ -1,8 +1,11 @@
-// gcc lib/glue.o -lstdc++ lib/libv8_libplatform.so lib/libv8_libbase.so lib/libicui18n.so lib/libicuuc.so lib/libv8.so  src/test.c -o bin/test.o
 #include <string.h>
 
 void main() {
   init();
   run("1+1");
+  run("1+1");
+  run("1+1");
+  run("1+1");
+  run("new Date()");
   finalyze();
 }
