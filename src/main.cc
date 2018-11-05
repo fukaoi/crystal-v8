baf639883bba0448cbc06bf0d60b86835d028881
fukaoi <goodakira7@gmail.com>
2018-11-05 23:44:43 +0900

    Refactoring

--- src/main.cc
+++ src/main.cc
@@ -5,20 +5,45 @@
 #include "v8.h"
 #include "unistd.h"
 
+using namespace v8;
+
+Isolate *isolate;
+Isolate::CreateParams create_params;
+
+void init() {
+  V8::InitializeICUDefaultLocation("argv[0]");
+  V8::InitializeExternalStartupData("argv[0]");
+
+  std::unique_ptr<Platform> platform = platform::NewDefaultPlatform();
+  V8::InitializePlatform(platform.get());
+  V8::Initialize();
+
+  create_params.array_buffer_allocator =
+  ArrayBuffer::Allocator::NewDefaultAllocator();
+  isolate = Isolate::New(create_params);
+}
+
+void deinit() {
+  isolate->Dispose();
+  V8::Dispose();
+  V8::ShutdownPlatform();
+  delete create_params.array_buffer_allocator;
+}
+
 int jslib(const char *code, v8::Isolate* isolate) {
   { 
-    v8::Isolate::Scope isolate_scope(isolate);
-    v8::HandleScope handle_scope(isolate);
-    v8::Local<v8::Context> context = v8::Context::New(isolate);
-    v8::Context::Scope context_scope(context);
-    v8::Local<v8::String> source =
-        v8::String::NewFromUtf8(isolate, code,
-                                v8::NewStringType::kNormal)
+    Isolate::Scope isolate_scope(isolate);
+    HandleScope handle_scope(isolate);
+    Local<Context> context = Context::New(isolate);
+    Context::Scope context_scope(context);
+    Local<String> source =
+        String::NewFromUtf8(isolate, code,
+                                NewStringType::kNormal)
             .ToLocalChecked();
-    v8::Local<v8::Script> script =
-        v8::Script::Compile(context, source).ToLocalChecked();
-    v8::Local<v8::Value> result = script->Run(context).ToLocalChecked();
-    v8::String::Utf8Value utf8(isolate, result);
+    Local<Script> script =
+        Script::Compile(context, source).ToLocalChecked();
+    Local<Value> result = script->Run(context).ToLocalChecked();
+    String::Utf8Value utf8(isolate, result);
     printf("%s\n", *utf8);
   }
   return 0;
@@ -26,34 +51,16 @@ int jslib(const char *code, v8::Isolate* isolate) {
 
 int main(int argc, char *argv[])
 {
-    // Initialize V8.
-  v8::V8::InitializeICUDefaultLocation(argv[0]);
-  v8::V8::InitializeExternalStartupData(argv[0]);
+  init();
 
-  std::unique_ptr<v8::Platform> platform = v8::platform::NewDefaultPlatform();
-  v8::V8::InitializePlatform(platform.get());
-  v8::V8::Initialize();
-  v8::Isolate::CreateParams create_params;
-  create_params.array_buffer_allocator =
-  v8::ArrayBuffer::Allocator::NewDefaultAllocator();
-  v8::Isolate* isolate = v8::Isolate::New(create_params);
-  jslib("2 * 2", isolate);
-  jslib("2 * 2", isolate);
+  jslib("-100 * -1000", isolate);
+  jslib("const i = 1; i + 100", isolate);
   jslib("2 * 2", isolate);
   jslib("10 + 20", isolate);
+  jslib("0.2 * 0.08", isolate);
+  jslib("class Person {constructor(name) {this.name = name;}sayHello() {return ('Hello, Im ' + this.getName());}getName() {return this.name;}}; new Person('山田敬三').sayHello();", isolate);
 
-  sleep(5);
-
-  jslib("10 + 20", isolate);
-  jslib("10 + 20", isolate);
-  jslib("10 + 20", isolate);
-  jslib("class Person {constructor(name) {this.name = name;}sayHello() {return ('Hello, Im ' + this.getName());}getName() {return this.name;}}; new Person('山田敬三').sayHello();", isolate);
-  jslib("class Person {constructor(name) {this.name = name;}sayHello() {return ('Hello, Im ' + this.getName());}getName() {return this.name;}}; new Person('山田敬三').sayHello();", isolate);
-  jslib("class Person {constructor(name) {this.name = name;}sayHello() {return ('Hello, Im ' + this.getName());}getName() {return this.name;}}; new Person('山田敬三').sayHello();", isolate);
-  isolate->Dispose();
-  v8::V8::Dispose();
-  v8::V8::ShutdownPlatform();
-  delete create_params.array_buffer_allocator;
+  deinit();
   return 0;
 }
 
