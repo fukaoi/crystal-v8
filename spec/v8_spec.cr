d14205195c5c29e11e734854fe5bb7fc9d336e33
fukaoi <goodakira7@gmail.com>
2018-11-11 01:39:38 +0900

    modified

--- spec/v8_spec.cr
+++ spec/v8_spec.cr
@@ -1,16 +1,14 @@
 require "./spec_helper"
 
+JS.init
 describe V8 do
-  # it "Run simple JS code" do
-  #   JS.init
-  #   JS.eval("10 + 20")
-  #   JS.eval("new Date()")
-  #   JS.eval("const a = 10;let b = 10;a + b;")
-  #   JS.destroy
-  # end
+  it "Run simple JS code" do
+    JS.eval("10 + 20")
+    JS.eval("new Date()")
+    JS.eval("const a = 10;let b = 10;a + b;")
+  end
 
   it "Run JS class or function code" do
-    JS.init
     JS.eval("class Person {
       constructor(name) {
         this.name = name;
@@ -24,7 +22,7 @@ describe V8 do
     }; new Person('Mike Davis').hello();")
     JS.eval("const fn = function(a){return a * 40};fn(10)")
     JS.eval("function exec(a) {return a * 50};exec(10)")
-    JS.destroy
   end
 end
+JS.destroy
 
