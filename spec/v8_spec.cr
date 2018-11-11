5ff0a2fb1e3a19851e42646414f7093b01f74e67
fukaoi <goodakira7@gmail.com>
2018-11-11 10:52:33 +0900

    renamed, wrapped, modified return value

--- spec/v8_spec.cr
+++ spec/v8_spec.cr
@@ -1,28 +1,28 @@
 require "./spec_helper"
 
-JS.init
 describe V8 do
   it "Run simple JS code" do
-    JS.eval("10 + 20")
-    JS.eval("new Date()")
-    JS.eval("const a = 10;let b = 10;a + b;")
+    v8 = V8::JS.new
+    result = v8.eval("10 + 20")
+    !p result
+    # JS.eval("new Date()")
+    # JS.eval("const a = 10;let b = 10;a + b;")
   end
 
-  it "Run JS class or function code" do
-    JS.eval("class Person {
-      constructor(name) {
-        this.name = name;
-      }
-      hello() {
-        return (\"Hello, I'm \" + this.getName());
-      }
-      getName() {
-        return this.name;
-      }
-    }; new Person('Mike Davis').hello();")
-    JS.eval("const fn = function(a){return a * 40};fn(10)")
-    JS.eval("function exec(a) {return a * 50};exec(10)")
-  end
+  # it "Run JS class or function code" do
+  #   JS.eval("class Person {
+  #     constructor(name) {
+  #       this.name = name;
+  #     }
+  #     hello() {
+  #       return (\"Hello, I'm \" + this.getName());
+  #     }
+  #     getName() {
+  #       return this.name;
+  #     }
+  #   }; new Person('Mike Davis').hello();")
+  #   JS.eval("const fn = function(a){return a * 40};fn(10)")
+  #   JS.eval("function exec(a) {return a * 50};exec(10)")
+  # end
 end
-JS.destroy
 
--- src/v8.cr
+++ src/v8.cr
@@ -9,12 +9,26 @@
   -lstdc++"
 )]
 
-lib JS
+lib LibV8
   fun init : Void
   fun eval(code : LibC::Char*) : LibC::Char*
   fun destroy : Void
 end
 
 module V8
+  class JS
+    def initialize
+      LibV8.init
+    end
+
+    def eval(code : String)
+      pointer = LibV8.eval(code)
+      String.new(pointer)
+    rescue exception
+      !pp "todo: #{exception}"
+    ensure
+      LibV8.destroy
+    end
+  end
 end
 
