f15c9f1a7e27ec1e692d2c309a66d1f38795fa3f
fukaoi <goodakira7@gmail.com>
2018-11-11 09:20:17 +0900

    no used run command and added spec file

---
+++ tasks/spec.cr
@@ -0,0 +1,14 @@
+require "lucky_cli"
+
+class Spec < LuckyCli::Task
+  banner "Spec all test and selectting test"
+
+  def call
+    if ARGV.size > 0
+      system("LD_LIBRARY_PATH=`pwd`/src/ext crystal spec -v #{ARGV[0]}")
+    else
+      system("LD_LIBRARY_PATH=`pwd`/src/ext crystal spec -v spec/")
+    end
+    puts "Done spec."
+  end
+end
