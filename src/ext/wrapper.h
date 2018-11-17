a2253ae710fdc871cb996835d01541bbadf51b44
fukaoi <goodakira7@gmail.com>
2018-11-17 23:46:45 +0900

    Refactoring

--- src/ext/wrapper.h
+++ src/ext/wrapper.h
@@ -12,9 +12,12 @@
 using namespace std;
 using namespace v8;
 
-extern "C" void init_icu(const char *external_file_path);
-extern "C" void init();
-extern "C" void destroy();
-extern "C" const char* eval(const char *src);
+extern "C" {
+  void init_icu(const char *external_file_path);
+  void init();
+  void destroy();
+  const char* eval(const char *src);
+}
 
+Local<Context> SetupCustomFunction();
 #endif
